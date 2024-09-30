98fcc7870dbb329b41de51157bf3812edc3c630d
Marco Antônio <ma.souza.junior@gmail.com>
2024-09-30 14:31:35 -0300

    enh: test github action

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  console.log('Creating a release');
+
   const token = core.getInput('token');
   const name = core.getInput('release_name');
   const body = core.getInput('release_body');
@@ -18,10 +20,14 @@ try {
     body: body,
   });
 
+  console.log(release);
+
   // Indicate success
   core.setOutput('status', 'success');
   core.setOutput('url', release.data.html_url);
   core.setOutput('tag', tag);
+
+  console.log('Release created successfully');
 
 } catch (error) {
   // Handle errors and indicate failure
