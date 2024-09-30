e3cc93a38b470571d83839f6afed82992065ea7c
Marco Antônio <ma.souza.junior@gmail.com>
2024-09-30 17:11:27 -0300

    fea: auto create releases when you app version change

--- index.ts
+++ index.ts
@@ -1,40 +1,113 @@
+import fs from "node:fs";
 import core from "@actions/core";
 import github from "@actions/github";
 
-try {
-  console.log("Creating a release");
-
+async function main() {
   const token = core.getInput("token");
-  const name = core.getInput("release_name");
-  const body = core.getInput("release_body");
+  const filename = core.getInput("version-file");
+
+  const content = fs.readFileSync(filename, "utf8");
+  const versionInfo = JSON.parse(content);
+  const releaseTag = versionInfo?.version;
+
+  if (!releaseTag) {
+    throw new Error("invalid version-file: missing version key");
+  }
 
   const octokit = github.getOctokit(token);
 
-  const tag = "1.0.0";
+  const allTagsSet = await getTagsSet(octokit);
+  const isNewTag = !allTagsSet.has(releaseTag);
+  if (isNewTag) {
+    console.log("Creating a new tag");
+    await createTag(releaseTag, octokit);
+  }
 
-  const release = await octokit.repos.createRelease({
+  const releaseResp = await getRelease(releaseTag, octokit);
+  if (releaseResp) {
+    console.log("Release already exists for tag", releaseTag);
+
+    core.setOutput("status", "exists");
+    core.setOutput("tag", releaseTag);
+    core.setOutput("url", releaseResp.data.url);
+
+    return;
+  }
+
+  console.log("Creating a new release for tag", releaseTag);
+  const createReleaseRes = await createRelease(releaseTag, octokit);
+
+  core.setOutput("status", "created");
+  core.setOutput("tag", releaseTag);
+  core.setOutput("url", createReleaseRes.data.url);
+
+  console.log("Release created successfully! 🚀");
+}
+
+async function getRelease(release: string, octokit: ReturnType<typeof github.getOctokit>) {
+  const resp = await octokit.request("GET /repos/{owner}/{repo}/releases/tags/{tag}", {
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-    tag_name: github.context.ref,
-    name: name,
-    body: body,
+    tag: release,
+  }).catch(err => {
+    if (err.status === 404) {
+      return null;
+    }
+    throw err;
   });
 
-  console.log({
+  return resp;
+}
+
+async function createTag(tag: string, octokit: ReturnType<typeof github.getOctokit>) {
+  const createTagResp = await octokit.request("POST /repos/{owner}/{repo}/git/tags", {
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-    tag_name: github.context.ref,
-    name: name,
-    body: body,
+    tag: tag,
+    message: `Release ${tag}`,
+    object: github.context.sha,
+    tagger: {
+      name: "GitHub Actions",
+      email: "octocat@github.com",
+      date: new Date().toISOString(),
+    },
+    type: "commit",
   });
 
-  // Indicate success
-  core.setOutput("status", "success");
-  core.setOutput("url", release.data.html_url);
-  core.setOutput("tag", tag);
+  if (createTagResp.status !== 201) {
+    throw new Error("Failed to create tag");
+  }
+}
 
-  console.log("Release created successfully");
-} catch (error) {
+async function createRelease(tag: string, octokit: ReturnType<typeof github.getOctokit>) {
+  const resp = await octokit.request("POST /repos/{owner}/{repo}/releases", {
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    tag_name: tag,
+    name: tag,
+    draft: false,
+    prerelease: false,
+    generate_release_notes: true,
+  });
+
+  if (resp.status !== 201) {
+    throw new Error("Failed to create release");
+  }
+
+  return resp;
+}
+
+async function getTagsSet(octokit: ReturnType<typeof github.getOctokit>) {
+  const tagsResp = await octokit.request("GET /repos/{owner}/{repo}/tags", {
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+  })
+
+  const allTags = tagsResp.data.map((tag) => tag.name);
+  return new Set(allTags);
+}
+
+await main().catch((error) => {
   // Handle errors and indicate failure
   core.setFailed(error.message);
-}
+})
