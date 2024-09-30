752e68fdce2bd8833c9eb73fbdc73bc6136e3860
Marco Antônio <ma.souza.junior@gmail.com>
2024-09-30 15:26:14 -0300

    fea: build from ts using bun

--- index.ts
+++ index.ts
@@ -1,1 +1,40 @@
-console.log("Hello via Bun!");
+import core from "@actions/core";
+import github from "@actions/github";
+
+try {
+  console.log("Creating a release");
+
+  const token = core.getInput("token");
+  const name = core.getInput("release_name");
+  const body = core.getInput("release_body");
+
+  const octokit = github.getOctokit(token);
+
+  const tag = "1.0.0";
+
+  const release = await octokit.repos.createRelease({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    tag_name: github.context.ref,
+    name: name,
+    body: body,
+  });
+
+  console.log({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    tag_name: github.context.ref,
+    name: name,
+    body: body,
+  });
+
+  // Indicate success
+  core.setOutput("status", "success");
+  core.setOutput("url", release.data.html_url);
+  core.setOutput("tag", tag);
+
+  console.log("Release created successfully");
+} catch (error) {
+  // Handle errors and indicate failure
+  core.setFailed(error.message);
+}
