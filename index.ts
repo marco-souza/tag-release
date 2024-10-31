54e753bf0a9ae744b92778a63a58b7fe74bff8f5
Marco Antônio <ma.souza.junior@gmail.com>
2024-10-31 17:09:40 -0300

    fea: accept title as input

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import github from "@actions/github";
 async function main() {
   const token = core.getInput("token");
   const filename = core.getInput("version-file");
+  const title = core.getInput("title");
 
   const content = fs.readFileSync(filename, "utf8");
   const versionInfo = JSON.parse(content);
@@ -35,7 +36,10 @@ async function main() {
   }
 
   console.log("Creating a new release for tag", releaseTag);
-  const createReleaseRes = await createRelease(releaseTag, octokit);
+  const createReleaseRes = await createRelease(
+    { title, tag: releaseTag },
+    octokit,
+  );
 
   core.setOutput("status", "created");
   core.setOutput("tag", releaseTag);
@@ -44,12 +48,18 @@ async function main() {
   console.log("Release created successfully! 🚀");
 }
 
-async function getRelease(release: string, octokit: ReturnType<typeof github.getOctokit>) {
-  const resp = await octokit.request("GET /repos/{owner}/{repo}/releases/tags/{tag}", {
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    tag: release,
-  }).catch(err => {
+async function getRelease(
+  release: string,
+  octokit: ReturnType<typeof github.getOctokit>,
+) {
+  const resp = await octokit.request(
+    "GET /repos/{owner}/{repo}/releases/tags/{tag}",
+    {
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      tag: release,
+    },
+  ).catch((err) => {
     if (err.status === 404) {
       return null;
     }
@@ -59,32 +69,47 @@ async function getRelease(release: string, octokit: ReturnType<typeof github.get
   return resp;
 }
 
-async function createTag(tag: string, octokit: ReturnType<typeof github.getOctokit>) {
-  const createTagResp = await octokit.request("POST /repos/{owner}/{repo}/git/tags", {
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    tag: tag,
-    message: `Release ${tag}`,
-    object: github.context.sha,
-    tagger: {
-      name: "GitHub Actions",
-      email: "octocat@github.com",
-      date: new Date().toISOString(),
+async function createTag(
+  tag: string,
+  octokit: ReturnType<typeof github.getOctokit>,
+) {
+  const createTagResp = await octokit.request(
+    "POST /repos/{owner}/{repo}/git/tags",
+    {
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      tag: tag,
+      message: `Release ${tag}`,
+      object: github.context.sha,
+      tagger: {
+        name: "GitHub Actions",
+        email: "octocat@github.com",
+        date: new Date().toISOString(),
+      },
+      type: "commit",
     },
-    type: "commit",
-  });
+  );
 
   if (createTagResp.status !== 201) {
     throw new Error("Failed to create tag");
   }
 }
 
-async function createRelease(tag: string, octokit: ReturnType<typeof github.getOctokit>) {
+type CreateRelease = {
+  tag: string;
+  title?: string;
+};
+
+async function createRelease({
+  tag,
+  title,
+}: CreateRelease, octokit: ReturnType<typeof github.getOctokit>) {
+  const name = title ? `${title} ${tag}` : tag;
   const resp = await octokit.request("POST /repos/{owner}/{repo}/releases", {
+    name,
+    tag_name: tag,
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-    tag_name: tag,
-    name: tag,
     draft: false,
     prerelease: false,
     generate_release_notes: true,
@@ -101,7 +126,7 @@ async function getTagsSet(octokit: ReturnType<typeof github.getOctokit>) {
   const tagsResp = await octokit.request("GET /repos/{owner}/{repo}/tags", {
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-  })
+  });
 
   const allTags = tagsResp.data.map((tag) => tag.name);
   return new Set(allTags);
@@ -110,4 +135,4 @@ async function getTagsSet(octokit: ReturnType<typeof github.getOctokit>) {
 await main().catch((error) => {
   // Handle errors and indicate failure
   core.setFailed(error.message);
-})
+});
